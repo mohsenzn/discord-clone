83d87b0d5315d84bc00794fba92331e7abd0a688
mohsen Zergni <mohsen.zergani@gmail.com>
2021-03-10 19:48:15 +0330

    Setup the Project

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h2>Discord</h2>
+      {/* sideBar */}
+      
+      {/* Chat */}
+    </div>
+  );
+}
+
+export default App;
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAgj6zpPNewKcamfQcBi4q8zRirKYRYJvw",
+//   authDomain: "discord-clone-9e22e.firebaseapp.com",
+//   projectId: "discord-clone-9e22e",
+//   storageBucket: "discord-clone-9e22e.appspot.com",
+//   messagingSenderId: "744061788644",
+//   appId: "1:744061788644:web:07cb8e2308fa94fdf9e7f1",
+//   measurementId: "G-739QWPDFF1"
+// };
