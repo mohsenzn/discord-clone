651ee813c885bc9ace85ffca12a9ed9b40e4f33e
mohsen Zergni <mohsen.zergani@gmail.com>
2021-03-11 16:40:08 +0330

    add firebasestone and firebaseDb , setup redux toolkit

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase';
+const firebaseConfig = {
+  apiKey: "AIzaSyAgj6zpPNewKcamfQcBi4q8zRirKYRYJvw",
+  authDomain: "discord-clone-9e22e.firebaseapp.com",
+  projectId: "discord-clone-9e22e",
+  storageBucket: "discord-clone-9e22e.appspot.com",
+  messagingSenderId: "744061788644",
+  appId: "1:744061788644:web:07cb8e2308fa94fdf9e7f1",
+  measurementId: "G-739QWPDFF1"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig); 
+
+const db = firebaseApp.firestore();
+
+const auth  = firebaseApp.auth();
+const provider = new firebase.auth.GithubAuthProvider();
+
+export {auth,provider};
+export default db;
