66034fae64d532ac908b5ab4bb11c9bdd3dcc205
mohsen Zergni <mohsen.zergani@gmail.com>
2021-03-12 17:39:43 +0330

    Finish To App

--- src/Components/Chat/Chat.js
+++ src/Components/Chat/Chat.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ChatHeader from "./ChatHeader";
 import AddCircleIcon from "@material-ui/icons/AddCircle";
 import CreditCardIcon from "@material-ui/icons/CreditCard";
@@ -8,34 +8,62 @@ import Message from "./Message";
 import { useSelector } from "react-redux";
 import { selectUser } from "../../features/userSlice";
 import { selectChannelId, selectChannelName } from "../../features/appSlice";
+import db from "../../firebase";
+import firebase from 'firebase'
 const Chat = () => {
   const user = useSelector(selectUser);
   const channelId = useSelector(selectChannelId);
-  const channelName =  useSelector(selectChannelName);
+  const channelName = useSelector(selectChannelName);
+
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    if (channelId) {
+      db.collection("channels")
+        .doc(channelId)
+        .collection("messages")
+        .orderBy("timestamp", "asc")
+        .onSnapshot((snapshot) =>
+          setMessages(snapshot.docs.map((doc) => doc.data()))
+        );
+    }
+  }, [channelId]);
+  const sendMessage = (e) => {
+    e.preventDefault()
+
+    db.collection('channels').doc(channelId).collection('messages').add({
+        message: input,
+        user: user,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp()
+    })
+
+    setInput("") 
+  }
   return (
     <div className="chat">
-      <ChatHeader channelName="" />
+      <ChatHeader channelName={channelName} />
 
       <div className="chat__messages">
-       <Message/>
+        {messages.map((message) => (
+          <Message
+            timestamp={message.timestamp}
+            message={message.message}
+            user={message.user}
+          />
+        ))}
       </div>
 
       <div className="chat__input">
         <AddCircleIcon fontsize="large" />
         <form>
           <input
-            disabled
-            value
-            onChange
+            disabled={!channelId}
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
             type="text"
-            placeholder
+            placeholder={`Send to #${channelName}`}
           />
-          <button
-            disabled
-            className="chat__inputButton"
-            type="submit"
-            onClick
-          >
+          <button disabled={!channelId} className="chat__inputButton" type="submit" onClick={sendMessage}>
             Send Message
           </button>
         </form>
--- src/Components/Chat/Message.js
+++ src/Components/Chat/Message.js
@@ -4,18 +4,18 @@ import { Avatar } from '@material-ui/core'
 function Message({timestamp, user, message}) {
     return (
         <div className="message">
-            <Avatar />
-            <div className="message__info">
-                <h4>
-                    
-                    <span className="message__timestamp">
-                        {new Date(timestamp?.toDate()).toUTCString()}
-                    </span>
-                </h4>
+        <Avatar src={user.photo}/>
+        <div className="message__info">
+            <h4>
+                {user.displayName}
+                <span className="message__timestamp">
+                    {new Date(timestamp?.toDate()).toUTCString()}
+                </span>
+            </h4>
 
-                <p></p>
-            </div>
+            <p>{message}</p>
         </div>
+    </div>
     )
 }
 
